71406cc15aee4711fbe6116431f8f57f640096c5
Will F <11763006+w4ester@users.noreply.github.com>
2025-02-06 15:22:13 -0500

    README update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 export default defineConfig({
   plugins: [react()],
-  base: https://github.com/w4ester/parabola-points  // This should match your repository name exactly
+  base: https://github.com/w4ester/parabola-points
 })
