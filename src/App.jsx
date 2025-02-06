5e864c5ae45458137caffe2e55a5f5d65a88fdfb
Will F <11763006+w4ester@users.noreply.github.com>
2025-02-06 14:43:09 -0500

    Initial parabola game commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+# File: src/App.jsx
+import ParabolaPointGame from './components/ParabolaPointGame'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <ParabolaPointGame />
+    </div>
+  )
+}
+
+export default App
